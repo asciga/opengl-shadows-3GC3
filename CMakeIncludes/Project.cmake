b77a2ef839cef6a45e7d772300d178d7e25b9e81
Alex <alexsci@hotmail.com>
2015-01-15 00:06:58 -0500

    glescraft now runs :)

--- CMakeIncludes/Project.cmake
+++ CMakeIncludes/Project.cmake
@@ -1,5 +1,8 @@
 set(MAIN_SOURCE 
-	${SOURCE_DIR}/main.cpp
+	${SOURCE_DIR}/glescraft.cpp
+	${SOURCE_DIR}/textures.c
+	${SOURCE_DIR}/common/shader_utils.cpp
 	)
 set(MAIN_HEADERS
+	${SOURCE_DIR}/common/shader_utils.h
 	)	
